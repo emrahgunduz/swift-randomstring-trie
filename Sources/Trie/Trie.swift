a9ec2e4cbc3d5c2db6308e830026340a6da6cd6c
Emrah Gunduz <emrah.gunduz@markakod.com>
2018-09-20 17:34:48 +0300

    Node init correction

--- Sources/Trie/Trie.swift
+++ Sources/Trie/Trie.swift
@@ -7,17 +7,16 @@ import Glibc
 import Foundation
 
 fileprivate class Node {
-  public var isEnd:    Bool = false
-  public var children: [Character: Node]
-
   public var parent:    Node?
   public var character: Character?
+  public var children:  [Character: Node]
+  public var isEnd:     Bool = false
 
-  public init (isEnd: Bool, parent: Node? = nil, character: Character? = nil) {
-    self.isEnd = isEnd
-    self.children = [:]
+  public init (parent: Node?, character: Character?, children: [Character: Node], isEnd: Bool) {
     self.parent = parent
     self.character = character
+    self.children = children
+    self.isEnd = isEnd
   }
 }
 
@@ -26,36 +25,7 @@ public class Trie {
   private let root: Node
 
   public init () {
-    self.root = Node(isEnd: false)
-  }
-
-  private func prefixNode (prefix: String) -> Node? {
-    var current: Node = self.root;
-
-    for letter: Character in prefix {
-      let next: Node? = current.children[letter]
-      if (next == nil) {
-        return nil
-      }
-      current = next!
-    }
-
-    return current
-  }
-
-  private func traverse (from: Node) -> String {
-    var current: Node?  = from
-    var item:    String = ""
-
-    while (current != nil) {
-      if (current!.character == nil) { break }
-      item = item + String(current!.character!)
-      current = current!.parent
-    }
-
-    let reversed = item.reversed() as [Character]
-
-    return reversed.map { String(describing: $0) }.joined()
+    self.root = Node(parent: nil, character: nil, children: [:], isEnd: false)
   }
 
   public func insert (element: String) -> Void {
@@ -65,7 +35,7 @@ public class Trie {
       for letter: Character in element {
         let next: Node? = current.children[letter]
         if (next == nil) {
-          current.children[letter] = Node(isEnd: false, parent: current, character: letter)
+          current.children[letter] = Node(parent: current, character: letter, children: [:], isEnd: false)
         }
 
         current = current.children[letter]!
@@ -107,29 +77,9 @@ public class Trie {
     }
   }
 
-  private func loop (from: Node, body: (String) -> Void) {
-    for (_, child) in from.children {
-      if (child.isEnd) {
-        body(self.traverse(from: child))
-      }
-
-      self.loop(from: child, body: body)
-    }
-  }
-
   public func contents (_ body: (String) -> Void) {
     queue.sync { [weak self] in
       loop(from: self!.root, body: body)
-    }
-  }
-
-  private func loopSimple (from: Node, body: () -> Void) {
-    for (_, child) in from.children {
-      if (child.isEnd) {
-        body()
-      }
-
-      self.loopSimple(from: child, body: body)
     }
   }
 
@@ -141,5 +91,57 @@ public class Trie {
       }
     }
     return all
+  }
+
+}
+
+fileprivate extension Trie {
+  fileprivate func prefixNode (prefix: String) -> Node? {
+    var current: Node = self.root;
+
+    for letter: Character in prefix {
+      let next: Node? = current.children[letter]
+      if (next == nil) {
+        return nil
+      }
+      current = next!
+    }
+
+    return current
+  }
+
+  fileprivate func traverse (from: Node) -> String {
+    var current: Node?  = from
+    var item:    String = ""
+
+    while (current != nil) {
+      if (current!.character == nil) { break }
+      item = item + String(current!.character!)
+      current = current!.parent
+    }
+
+    let reversed = item.reversed() as [Character]
+
+    return reversed.map { String(describing: $0) }.joined()
+  }
+
+  fileprivate func loop (from: Node, body: (String) -> Void) {
+    for (_, child) in from.children {
+      if (child.isEnd) {
+        body(self.traverse(from: child))
+      }
+
+      self.loop(from: child, body: body)
+    }
+  }
+
+  fileprivate func loopSimple (from: Node, body: () -> Void) {
+    for (_, child) in from.children {
+      if (child.isEnd) {
+        body()
+      }
+
+      self.loopSimple(from: child, body: body)
+    }
   }
 }
